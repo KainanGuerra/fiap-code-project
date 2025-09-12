aaa07a123a6046bbe533595f58cbd92d8c6e1aa7
KaueLz <kaueluiz@outlook.com.br>
2025-09-11 21:38:54 -0300

    Fix query page

--- src/modules/blog/post/dto/get-posts-dto.ts
+++ src/modules/blog/post/dto/get-posts-dto.ts
@@ -1,8 +1,11 @@
+import { Exclude, Expose } from 'class-transformer';
 import { IsOptional, IsString, MinLength } from 'class-validator';
 
+@Exclude()
 export class GetPostsDTO {
   @IsOptional()
   @IsString()
   @MinLength(3)
+  @Expose()
   term?: string;
 }
